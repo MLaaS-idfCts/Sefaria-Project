caafaaf2bf7ffa5c266937bc71b19b07c7e0f684
yishairasowsky <yishairasowsky@gmail.com>
2020-06-16 15:06:12 +0000

    make basic classifier using linear svc set up data for including ref in analysis

---
+++ ML/scripts/get_section.py
@@ -0,0 +1,32 @@
+    def get_section(input_string):
+        """
+        Given a string, produce the substring that lies 
+        after the last comma (if any) but 
+        before the numbers at the end (if any).
+        """
+        result = ''
+        
+        # get rid of everything before last comma
+        after_comma = input_string # init
+        last_comma = input_string.rfind(', ')
+        if last_comma != -1:
+            after_comma = input_string[last_comma + 2:]
+
+        # keep only letters and spaces
+        result = ''.join(char for char in after_comma if char.isalpha() or char == ' ')
+
+        # remove single chars
+        result = ' '.join( [w for w in result.split() if len(w)>1] )
+        return result
+
+    input_list = [
+            "Gittin 78a:1",
+            "Shulchan Arukh, Even HaEzer 139:15",
+            "Mishneh Torah, Rest on a Holiday 6:12",
+            "Beitzah 17b:13",
+            "Mishneh Torah, Sabbath 22:10"
+        ]
+
+    for input in input_list: 
+        output = get_section(input)
+        print(output)

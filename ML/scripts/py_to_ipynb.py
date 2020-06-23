500660433d74aea8312757c03c1440275462adbf
yishai <yishairasowsky@gmail.com>
2020-06-23 15:17:19 +0300

    del py file for converting py to nb

--- ML/scripts/py_to_ipynb.py
+++
@@ -1,6 +0,0 @@
-import IPython.nbformat.current as nbf
-py_name = 'py_to_ipynb'
-nb_name = 'py_to_ipynb'
-# notebooks\multi_label.ipynb
-nb = nbf.read(open(f'{py_name}.py', 'r'), 'py')
-nbf.write(nb, open(f'notebooks/{nb_name}.ipynb', 'w'), 'ipynb')

8baad0adc877f240d6969ed26b21a62d88aa5434
root <root@yishai-remotedocker-0.yishai-remotedocker.default.svc.cluster.local>
2020-07-06 11:12:06 +0000

    simplify and comment code

--- ML/scripts/multi_label_classification.py
+++ ML/scripts/multi_label_classification.py
@@ -1,107 +1,134 @@
+# IMPORT LIBRARIES
 import sys
+import time
+import scipy
+import warnings
 import numpy as np
 import pandas as pd
 
 from tqdm import tqdm
-from datetime import datetime
-from sklearn.utils import shuffle
-from skmultilearn.adapt import MLkNN
-from sklearn.pipeline import Pipeline
-from sklearn.svm import SVC, LinearSVC
-# from fastxml import Trainer, Inferencer
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from scipy.sparse import csr_matrix, lil_matrix
-from sklearn.datasets import make_classification
 from classes import DataManager, PipelineFactory
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.linear_model import LogisticRegression, SGDClassifier
+from datetime import datetime
+from sklearn.svm import SVC, LinearSVC
+from scipy.sparse import csr_matrix, lil_matrix
+from sklearn.utils import shuffle
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import make_classification
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.multiclass import OneVsRestClassifier
+from skmultilearn.adapt import BRkNNaClassifier, MLkNN
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
 from skmultilearn.problem_transform import LabelPowerset, BinaryRelevance, ClassifierChain
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+# for timing processes
+start_time = datetime.now()
+
+# choose convenient settings
+# ignore warnings regarding column assignment, 
+# e.g. df['col1'] = list1 -- not 100% sure about this
 pd.options.mode.chained_assignment = None  # default='warn'
+
+# do not limit num of rows in df to display
 pd.set_option('display.max_rows', None)
 
-NUM_TOPICS = 5
-ROW_LIMIT = 10000
-DATA_PATH = '/persistent/Sefaria-Project/ML/data/yishai_data.csv'
+# disable future warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-classifier_types = {
-    1:OneVsRestClassifier(SVC()),
-    2:OneVsRestClassifier(LinearSVC()),
-    # 3:OneVsRestClassifier(LogisticRegression()),
-    # 4:OneVsRestClassifier(MultinomialNB()),
-    # 5:OneVsRestClassifier(GaussianNB()),
-    
-    # 6:BinaryRelevance(SVC()),
-    7:BinaryRelevance(LinearSVC()),
-    8:BinaryRelevance(LogisticRegression()),
-    9:BinaryRelevance(MultinomialNB()),
-    10:BinaryRelevance(GaussianNB()),
-    
-    11:ClassifierChain(SVC()),
-    12:ClassifierChain(LinearSVC()),
-    13:ClassifierChain(LogisticRegression()),
-    14:ClassifierChain(MultinomialNB()),
-    15:ClassifierChain(GaussianNB()),
-    
-    16:LabelPowerset(SVC()),
-    17:LabelPowerset(LinearSVC()),
-    18:LabelPowerset(LogisticRegression()),
-    19:LabelPowerset(MultinomialNB()),
-    20:LabelPowerset(GaussianNB()),
-    
-}
-
+# width of column to dispaly in dataframe
 pd.options.display.max_colwidth = 50
 
+# how many topics to consider
+NUM_TOPICS = 3
+
+# max num of passages to examine
+ROW_LIMIT = 1000
+
+# location of actual csv
+DATA_PATH = '/persistent/Sefaria-Project/ML/data/yishai_data.csv'
+
+# dict of classifier types
+classifier_types = {
+    1:OneVsRestClassifier(LogisticRegression())
+}
+
+# load original data 
 df = pd.read_csv(DATA_PATH)[:ROW_LIMIT]
 
+# init class to manage data
 data = DataManager(raw_df = df, num_topics = NUM_TOPICS, should_clean = True, should_stem = False)
 
+# list of most commonly occurring topics
 top_topics = data._get_top_topics()
 
-result = data.preprocess_dataframe()
+# data in usable fromat, e.g. cleaned, stemmed, etc.
+# e.g. should have column for passage text, and for each topic
+data = data.preprocess_dataframe()
 
-data = result
-
+# randomly split into training and testing sets
 train, test = train_test_split(data, random_state=42, test_size=0.30, shuffle=True)
 
+# init column for topic predictions
 test['pred_topics'] = None
 
+# select just the words of each passage
 train_text = train['passage_text']
 test_text = test['passage_text']
 
+# init a vectorizer that will convert string of words into numerical format
 vectorizer = TfidfVectorizer(strip_accents='unicode', analyzer='word', ngram_range=(1,3), norm='l2')
-# only fit with training data, not with testing data, because testing data should be "UNSEEN"
+
+# convert words of each passages
+# Note: We only fit with training data, 
+# but NOT with testing data, because testing data should be "UNSEEN"
 vectorizer.fit(train_text)
 
 # numerical version of words in passage
 x_train = vectorizer.transform(train_text)
-# topics columns with 1 in row if that passage belongs
-y_train = train.drop(labels = ['passage_text','true_topics'], axis=1)
-
 x_test = vectorizer.transform(test_text)
+
+# topics columns, with 0/1 indicating if the topic of this column relates to that row's passage
+y_train = train.drop(labels = ['passage_text','true_topics'], axis=1)
 y_test = test.drop(labels = ['passage_text','true_topics'], axis=1)
 
+# loop thru various classifier types
 for classifier_key in classifier_types.keys():
+    
     # choose your classifier type
-
     classifier = classifier_types[classifier_key]
-
+    
+    # train the classifier
     classifier.fit(x_train, y_train)
+
+    # make predictions
     test_predictions = classifier.predict(x_test)
     train_predictions = classifier.predict(x_train)
 
+    # convert csc matrix into list of csr matrices, e.g. preds_list = [[0,1,0,1,0],[0,0,0,1,0],...,[1,0,0,0,0]]
+    # one matrix per passage, e.g. [0,0,0,1,1] inidicates this passage corrseponds to the last two topics
     preds_list = list(test_predictions)
+    
+    # init list of sublists, one sublist per passage, 
+    # e.g. pred_labels_list = [['prayer'],['prayer','judges'],['moses']]
+    
     pred_labels_list = []
+    # loop thru each matrix in list, again one matrix per passage, 
+    
     for array in preds_list:
+    
+        if isinstance(array,scipy.sparse.csr.csr_matrix):
+            array = array.tolil().data.tolist()
+    
+        # init topics list for this row, e.g. passage_labels = ['prayer', 'moses']
         passage_labels = []
-        for topic_index, pred_value in enumerate(array):
+    
+    
+        # if 1 occurs in ith element in the array, record ith topic
+        for topic_index, pred_value in enumerate(list(array)):
             if pred_value != 0:
                 passage_labels.append(top_topics[topic_index])
         pred_labels_list.append(passage_labels)
@@ -112,7 +139,7 @@ for classifier_key in classifier_types.keys():
 
     result = test[cols]
     topics_comparison = test[['true_topics','pred_topics']]
-    # print(topics_comparison)
+    print(topics_comparison)
     print('\nClassifier type:',classifier)
     print('**********************************************')
 
@@ -199,5 +226,9 @@ for classifier_key in classifier_types.keys():
     #     print(k)
         # print(v)
 
+    # show how much time has elapsed
+    print(datetime.now()-start_time)
+    
 print([(topic_index, topic) for topic_index, topic in enumerate(top_topics)])
 print()
+# """
